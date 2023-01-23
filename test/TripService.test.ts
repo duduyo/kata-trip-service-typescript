1fb1840941ff2c9f7a2842c0069df5ef39da44dd
Lionel DURAND <lioneldurand@gmail.com>
2023-01-23 20:58:09 +0100

    Deuxième test

--- test/TripService.test.ts
+++ test/TripService.test.ts
@@ -2,13 +2,24 @@ import { describe, it, expect } from 'vitest'
 import TripService from "../src/trip/TripService";
 import User from "../src/user/User";
 import UserNotLoggedInException from "../src/exception/UserNotLoggedInException";
+import Trip from "../src/trip/Trip";
 
 const NOT_LOGGED_USER = null;
-const A_USER = new User();
+const LOGGED_USER = new User();
+
+const NOT_A_FRIEND = new User();
+
+const NO_TRIPS: Trip[] = [];
+
 describe("TripService", () => {
 
+    const tripService = new TripService();
+
     it("Should throw exception when user is not logged in", () => {
-        const tripService = new TripService();
-        expect(() => tripService.getTrips(NOT_LOGGED_USER, A_USER)).to.throw(UserNotLoggedInException)
+        expect(() => tripService.getTrips(NOT_LOGGED_USER, NOT_A_FRIEND)).to.throw(UserNotLoggedInException)
     });
+    it("Should return no trips when users are not friends", () => {
+        expect(tripService.getTrips(LOGGED_USER, NOT_A_FRIEND)).to.have.ordered.members(NO_TRIPS)
+    });
+
 });
